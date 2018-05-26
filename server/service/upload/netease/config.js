15f1e47d9a3bd897fe69907e9dcffec5f0b97f0a
hzliaobolin <hzliaobolin@corp.netease.com>
2018-05-26 15:49:50 +0800

    :art: 忽略用户自定义服务

--- server/service/upload/netease/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    accessId: process.env.accessId,
-    accessSecret: process.env.accessSecret,
-    host: process.env.host,
-    cdnHost: process.env.cdnHost,
-    port: process.env.port,
-    bucket: process.env.bucket,
-    username: process.env.username
-};
